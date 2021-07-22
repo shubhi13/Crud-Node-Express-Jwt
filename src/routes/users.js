dc826de36c3e38dba412a6207804ee1faa374211
Shubhi Srivastava <72998724+shubhi13@users.noreply.github.com>
2021-07-23 02:59:16 +0530

    Delete src directory

--- src/routes/users.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express")
-const router = Router()
-
-const userController = require("../controllers/users")
-
-router.post("/", userController.authenticate)
-
-module.exports = router
